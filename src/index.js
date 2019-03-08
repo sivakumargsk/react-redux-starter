2662d8cd2503a4ca1d5c879df210c1f92fb44c37
Siva Kumar <geddasiva777@gmail.com>
2019-03-08 16:24:50 +0530

    Add Redux-thunk for asynchronous flow in Redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from './rootReducer';
 import AppRouter from './AppRouter';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 render(
   <Provider store={store}>
